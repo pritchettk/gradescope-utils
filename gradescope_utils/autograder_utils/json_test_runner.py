312c09f6eeb1cc3c0c0af3777e7a90b14dfdc268
Isaac Levy <isaac.r.levy@gmail.com>
2022-09-18 16:46:01 -0400

    Adjust msg spacing

--- gradescope_utils/autograder_utils/json_test_runner.py
+++ gradescope_utils/autograder_utils/json_test_runner.py
@@ -77,7 +77,7 @@ class JSONTestResult(result.TestResult):
             if hide_errors_message:
                 output += hide_errors_message
             else:
-                output += f'FAIL: {err[1] or "<no msg>"}\n'
+                output += f'\nFAIL: {err[1] or "<no msg>"}\n'
         result = {
             "name": self.getDescription(test),
         }
