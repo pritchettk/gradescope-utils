5e71552691c9ccfc876f21a31585cf5ef8831e91
Pritchett, Kory Joseph <kpritche@purdue.edu>
2023-11-13 15:11:02 -0500

    Improved error handling for subtests

--- gradescope_utils/autograder_utils/json_test_runner.py
+++ gradescope_utils/autograder_utils/json_test_runner.py
@@ -225,19 +225,22 @@ class JSONTestRunner(object):
         if self.post_processor is not None:
             self.post_processor(self.json_data)
 
-        if ("merge_subtests" in self.json_data["tests"] 
-            and len(self.json_data["tests"]) > 1):
-            
-            i = 0
-            while i < len(self.json_data["tests"]):
-                if (self.json_data["tests"][i]["name"] == self.json_data["tests"][i+1]["name"]
-                    and self.json_data["tests"][i]["merge_subtests"] == 'True'):
-                    self.json_data["tests"][i]["output"] += self.json_data["tests"][i+1]["output"]
-                    del self.json_data["tests"][i+1]
-                else:
-                    i += 1
-                    if i + 1 >= len(self.json_data["tests"]):
-                        break
+        
+        try:
+            if len(self.json_data["tests"]) > 1:
+
+                i = 0
+                while i < len(self.json_data["tests"]):
+                    if (self.json_data["tests"][i]["name"] == self.json_data["tests"][i+1]["name"]
+                        and self.json_data["tests"][i]["merge_subtests"] == 'True'):
+                        self.json_data["tests"][i]["output"] += self.json_data["tests"][i+1]["output"]
+                        del self.json_data["tests"][i+1]
+                    else:
+                        i += 1
+                        if i + 1 >= len(self.json_data["tests"]):
+                            break
+        except KeyError:
+            pass
             
         json.dump(self.json_data, self.stream, indent=4)
         self.stream.write('\n')
