4dbb9b1b293963d1a77f910e12d27e7d54cd3d7b
Pritchett, Kory Joseph <kpritche@purdue.edu>
2023-11-13 13:47:03 -0500

    Update json_test_runner.py

--- gradescope_utils/autograder_utils/json_test_runner.py
+++ gradescope_utils/autograder_utils/json_test_runner.py
@@ -165,7 +165,7 @@ class JSONTestRunner(object):
                  failfast=False, buffer=True, visibility=None,
                  stdout_visibility=None, post_processor=None,
                  failure_prefix="Test Failed: ",
-                 merge_subtests=False):
+                 merge_subtests=True):
         """
         Set buffer to True to include test output in JSON
 
